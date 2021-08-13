f68e1fa89189426003020d973e1f6f0205419bb8
nxforg <christopher.anstead@yahoo.co.uk>
2021-08-13 19:15:22 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <NavBar title="Chris Anstead"/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/Nav.vue';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    NavBar
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+*{
+  font-family: 'Montserrat', sans-serif;
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,57 @@
+<template>
+<div id="app">
+ <v-app>
+  <div>
+    <v-app-bar color="blue-grey darken-3" dense dark>
+    <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+    <v-toolbar-title>{{title}}</v-toolbar-title>
+    <v-spacer> </v-spacer>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+        <v-list>
+      <v-list-item v-for="n in icons" :key="n" @click="() => {}" :href="n.url">
+       <v-list-item-icon>
+        <v-icon>{{n.ico}}</v-icon>
+       </v-list-item-icon><v-list-item-title>{{ n.label }}</v-list-item-title>
+      </v-list-item>
+     </v-list>
+    </v-navigation-drawer>
+  </div>
+ </v-app>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+    title: String
+  },
+      data: () => ({
+       drawer: false,
+       group: null,
+       icons: [
+       {
+         ico: 'mdi-facebook',
+         url: 'https://www.facebook.com/christopher.anstead',
+         label: 'Facebook',
+       },
+       {
+         ico: 'mdi-twitter',
+         url: 'https://twitter.com/NX_FORG',
+         label: 'Twitter',
+       },
+       {
+         ico: 'mdi-linkedin',
+         url: 'https://www.linkedin.com/in/christopher-anstead-7b2072a8/',
+         label: 'LinkedIn',
+       },
+       {
+         ico: 'mdi-code-greater-than-or-equal',
+         url: 'https://github.com/NXFORG',
+         label: 'GitHub',
+       },
+       ]
+     })
+    }
+</script>
